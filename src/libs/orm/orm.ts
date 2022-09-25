6092f8ca5d70a0c9b8c963d175345c8399416b2b
backend-junsung <leejs1030@cashwalk.io>
2022-09-25 21:16:10 +0900

    feat: orm과 query builder를 분리

--- src/libs/orm/orm.ts
+++ src/libs/orm/orm.ts
@@ -1,66 +1,34 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import { DbConnection } from '../../connection/db-connection';
 import { DocumentConnection } from '../../connection/document-connection';
-import { ParseSchema } from '../../schema/parse/parse-schema';
-import { DynamoSchemaError } from '../../errors';
-import { IndexFinder } from './index-finder';
-import { AllowedKeyTypes, DynamoIndex, FindInput, Operator, PrimaryKey } from '../../typing/typing';
+import { AllowedKeyTypes, FindInput } from '../../typing/typing';
+import { QueryBuilder } from './query-builder';
 
 export class Orm {
-  private readonly indexFinder: IndexFinder;
-  private readonly tableName: string;
-  private readonly primaryKey: PrimaryKey;
-  private readonly globalIndices?: DynamoIndex[];
-  private readonly localIndices?: DynamoIndex[];
+  private readonly queryBuilder: QueryBuilder;
 
   constructor(
     schema: DocumentClient.CreateTableInput,
     private readonly dynamoDb: DbConnection,
     private readonly documentClient: DocumentConnection,
   ) {
-    const parseSchema = new ParseSchema(schema);
-
-    this.tableName = schema.TableName;
-    this.primaryKey = parseSchema.parsePrimaryKey();
-    this.globalIndices = parseSchema.parseGlobalIndex();
-    this.localIndices = parseSchema.parseLocalIndex();
-
-    this.indexFinder = new IndexFinder(this.primaryKey, this.globalIndices, this.localIndices);
+    this.queryBuilder = new QueryBuilder(schema);
   }
 
   async findUnique(params: { where: { [key: string]: AllowedKeyTypes } }) {
-    const { where } = params;
-    const keys = Object.keys(where);
-    const values = keys.map((key) => where[key]);
+    const query = this.queryBuilder.buildGetQueryParams(params);
+    return this.documentClient.get(query);
+  }
 
-    if (!this.indexFinder.isPrimaryKey(keys, values))
-      throw new DynamoSchemaError('this is not primary key!');
-
-    return this.documentClient.get({ TableName: this.tableName, Key: where });
+  private canBeQueried(
+    params: DocumentClient.QueryInput | DocumentClient.ScanInput,
+  ): params is DocumentClient.QueryInput {
+    return !!(params.IndexName || (params as any).KeyConditionExpression);
   }
 
   async find(params: FindInput): Promise<any> {
-    const { where } = params;
-    const and = where[Operator.and];
-    const or = where[Operator.or];
-
-    delete where[Operator.and];
-    delete where[Operator.or];
-
-    const keys = JSON.stringify(where) !== '{}' ? where : null;
-
-    let keysCount = 0;
-    if (and) keysCount++;
-    if (or) keysCount++;
-    if (keys) keysCount++;
-
-    if (keysCount > 1) throw new DynamoSchemaError('to many where conditions!');
-
-    let result;
-    if (and) result = and;
-    if (or) result = or;
-    if (keys) result = keys;
-
-    return result;
+    const query = this.queryBuilder.buildQueryQueryParams(params);
+    if (this.canBeQueried(query)) return this.documentClient.query(query);
+    return this.documentClient.scan(query);
   }
 }
---
+++ src/libs/orm/query-builder.ts
@@ -0,0 +1,62 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { ParseSchema } from '../../schema/parse/parse-schema';
+import { IndexFinder } from './index-finder';
+import { AllowedKeyTypes, DynamoIndex, FindInput, Operator, PrimaryKey } from '../../typing/typing';
+import { DynamoSchemaError } from '../../errors';
+
+export class QueryBuilder {
+  private readonly indexFinder: IndexFinder;
+  private readonly tableName: string;
+  private readonly primaryKey: PrimaryKey;
+  private readonly globalIndices?: DynamoIndex[];
+  private readonly localIndices?: DynamoIndex[];
+
+  constructor(schema: DocumentClient.CreateTableInput) {
+    const parseSchema = new ParseSchema(schema);
+
+    this.tableName = schema.TableName;
+    this.primaryKey = parseSchema.parsePrimaryKey();
+    this.globalIndices = parseSchema.parseGlobalIndex();
+    this.localIndices = parseSchema.parseLocalIndex();
+
+    this.indexFinder = new IndexFinder(this.primaryKey, this.globalIndices, this.localIndices);
+  }
+
+  buildGetQueryParams(params: {
+    where: { [key: string]: AllowedKeyTypes };
+  }): DocumentClient.GetItemInput {
+    const { where } = params;
+    const keys = Object.keys(where);
+    const values = keys.map((key) => where[key]);
+
+    if (!this.indexFinder.isPrimaryKey(keys, values))
+      throw new DynamoSchemaError('this is not primary key!');
+
+    return { TableName: this.tableName, Key: where };
+  }
+
+  buildQueryQueryParams(params: FindInput): DocumentClient.QueryInput | DocumentClient.ScanInput {
+    const { where } = params;
+    const and = where[Operator.and];
+    const or = where[Operator.or];
+
+    delete where[Operator.and];
+    delete where[Operator.or];
+
+    const keys = JSON.stringify(where) !== '{}' ? where : null;
+
+    let keysCount = 0;
+    if (and) keysCount++;
+    if (or) keysCount++;
+    if (keys) keysCount++;
+
+    if (keysCount > 1) throw new DynamoSchemaError('to many where conditions!');
+
+    let result;
+    if (and) result = and;
+    if (or) result = or;
+    if (keys) result = keys;
+
+    return result as any;
+  }
+}
